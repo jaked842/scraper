97a8097e025a658e9004427aee05fd75c7e2db06
Admin <admin@MacBook-Pro.local>
2021-05-21 17:04:51 -0400

    initial

---
+++ scraper.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+PATH = '/Users/admin/Downloads/chromedriver'
+driver = webdriver.Chrome(PATH)
+
+
+driver.get('https://www.federalpremium.com/rifle/fusion/')
+
+tracking_consent_button = driver.find_element_by_class_name('affirm')
+tracking_consent_button.click()
+
+rifle = driver.find_element_by_link_text('Rifle')
+rifle.click()
+
+
+creedmoor_div = driver.find_element_by_id('header-refinement-caliber')
+creedmoor_div.click()
+
+
+creedmoor_list = driver.find_element_by_xpath(".//label[@for='6.5 Creedmoor']")
+creedmoor_list.click()
+
+time.sleep(5)
+
+products = driver.find_elements_by_class_name('col-lg-4')
+
+
+for x in range(12):
+    if products[x].find_element_by_class_name('label-1').text == 'Currently Unavailable':
+        continue
+    else:
+        nme = products[x].find_element_by_class_name('pdp-link').text
+        prce = products[x].find_element_by_class_name('value').text
+
+        print(f'{nme}')
